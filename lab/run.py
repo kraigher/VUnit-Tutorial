0d331e5d664a29bc7149450e64f607bc3232daa4
Olof Kraigher <olof.kraigher@gmail.com>
2018-11-10 17:46:33 +0100

    add osvvm

--- lab/run.py
+++ lab/run.py
@@ -2,6 +2,7 @@ from os.path import join, dirname
 from vunit import VUnit
 
 prj = VUnit.from_argv()
+prj.add_osvvm()
 tlv = prj.add_library("tlv")
 tlv.add_source_files(join(dirname(__file__), "*.vhd"))
 prj.set_compile_option("ghdl.flags", ["-Wno-hide"])
